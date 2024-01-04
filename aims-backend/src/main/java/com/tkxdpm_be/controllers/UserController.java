5a6ecae0d0359a035f0ed6a7809edc2fc5cff361
khanhpd2002 <khanh.pd2002@gmail.com>
2024-01-04 22:33:06 +0700

    update login, sort

--- aims-backend/src/main/java/com/tkxdpm_be/controllers/UserController.java
+++ aims-backend/src/main/java/com/tkxdpm_be/controllers/UserController.java
@@ -2,10 +2,13 @@ package com.tkxdpm_be.controllers;
 
 
 import com.tkxdpm_be.entities.User;
+import com.tkxdpm_be.models.requests.LoginRequest;
 import com.tkxdpm_be.services.UserService;
 import lombok.RequiredArgsConstructor;
+import model.BaseResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import utils.ApiException;
 
 import java.util.List;
 
@@ -21,5 +24,12 @@ public class UserController {
     public ResponseEntity<List<User>> getAllUser () {
         List<User> user = userService.findAllUser();
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public BaseResponse<User> login(@RequestBody LoginRequest request) throws ApiException {
+        BaseResponse<User> response = new BaseResponse<>();
+        response.setData(this.userService.login(request));
+        return response;
     }
 }
